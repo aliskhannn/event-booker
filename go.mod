a7797d72887255c3fc3a25b51b37be959a3ec94f
Aliskhan <aliskhanreventon@gmail.com>
2025-09-24 18:05:17 +0300

    feat(repository): add CreateUser, GetUserByEmail, ExistsByEmail methods

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/aliskhannn/event-booker
 
 go 1.25.1
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/wb-go/wbf v0.0.4
+)
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ internal/repository/user/repo.go
@@ -0,0 +1,83 @@
+package user
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/wb-go/wbf/dbpg"
+
+	"github.com/aliskhannn/event-booker/internal/model"
+)
+
+var ErrUserNotFound = errors.New("user not found")
+
+// Repository provides methods to interact with users table.
+type Repository struct {
+	db *dbpg.DB
+}
+
+// NewRepository creates a new user repository.
+func NewRepository(db *dbpg.DB) *Repository {
+	return &Repository{db: db}
+}
+
+// CreateUser adds a new user to the database.
+func (r *Repository) CreateUser(ctx context.Context, user *model.User) (uuid.UUID, error) {
+	query := `
+		INSERT INTO users (email, password_hash, name)
+		VALUES ($1, $2, $3)
+		RETURNING id;
+	`
+
+	err := r.db.Master.QueryRowContext(
+		ctx, query, user.Email, user.PasswordHash,
+	).Scan(&user.ID)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return user.ID, nil
+}
+
+// GetUserByEmail retrieves a user by email.
+func (r *Repository) GetUserByEmail(ctx context.Context, email string) (*model.User, error) {
+	query := `
+		SELECT id, email, password_hash, name, created_at
+		FROM users
+		WHERE email = $1
+	`
+
+	var user model.User
+	err := r.db.Master.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.Email,
+		&user.PasswordHash,
+		&user.Name,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrUserNotFound
+		}
+
+		return nil, fmt.Errorf("failed to get user by email: %w", err)
+	}
+
+	return &user, nil
+}
+
+// ExistsByEmail checks if a user with the given email already exists in the database.
+func (r *Repository) ExistsByEmail(ctx context.Context, email string) (bool, error) {
+	query := `SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)`
+
+	var exists bool
+	err := r.db.Master.QueryRowContext(ctx, query).Scan(&exists)
+	if err != nil {
+		return false, fmt.Errorf("failed to check if user exists: %w", err)
+	}
+
+	return exists, nil
+}
